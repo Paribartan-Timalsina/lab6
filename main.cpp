aed75d7c075535bde5de358becb0abffdce8ca1d
Paribartan <timalsinapari015@gmail.com>
2022-11-26 22:04:25 +0545

    files added

--- main.cpp
+++ main.cpp
@@ -1,22 +1,37 @@
-#include<iostream>
-#include<random>
-#include<time.h>
-#include<vector>
+#include <iostream>
+#include <random>
+#include <time.h>
+#include <vector>
+#include <chrono>
 using namespace std;
-#define MAX_SIZE 10
-void quickSort(vector<int> &vec,int low,int high);
-int main(){
-    std::vector<int> vec;
-srand(time(NULL));
-for(int i=0;i<MAX_SIZE;i++){
-vec.push_back(rand()%200);
-}
-for(int i=0;i<MAX_SIZE;i++){
-cout<<vec[i]<<endl;
-}
-quickSort(vec,0,MAX_SIZE);
-cout<<"after sorting"<<endl;
-for(int i=0;i<MAX_SIZE;i++){
-cout<<vec[i]<<endl;
-}
+using namespace std::chrono;
+#define MAX_SIZE 10000
+void quickSort(vector<int> &vec, int low, int high);
+int main()
+{
+  int j;
+  std::vector<int> vec;
+  srand(time(NULL));
+  for (j = MAX_SIZE; j <= 10 * MAX_SIZE; j = j + MAX_SIZE)
+  {
+
+    for (int i = 0; i < j; i++)
+    {
+      vec.push_back(rand() % 200);
+    }
+  //  cout<<"Unsorted data"<<endl;
+  //   for(int i=0;i<j;i++){
+  //   cout<<vec[i]<<endl;
+  //   }
+    auto start = high_resolution_clock::now();
+   quickSort(vec, 0, j - 1);
+    auto stop = high_resolution_clock::now();
+// cout<<"sorted data"<<endl;
+//     for(int i=0;i<j;i++){
+//     cout<<vec[i]<<endl;
+//     }
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << " (Quick Sort) Time taken by function: "
+         << duration.count()/1000<< " milliseconds" << " for the "<<j<<" values"<<endl;
+  }
 }
--- sort.cpp
+++ sort.cpp
@@ -1,33 +1,31 @@
-#include<iostream>
-#include<vector>
-int partition(std::vector<int> & vec,int low,int high){
-    int pivot=vec[low];
-    int i=low;
-    int j=high+1;
-    while(i<j){
-        while(vec[j]<pivot){
-            j=j-1;
-        }
-         while(vec[i]>pivot){
+#include <iostream>
+#include <vector>
+int partition(std::vector<int> & vec, int low, int high)
+{
+    int pivot = vec[high];
+    int i = low-1;
+    for(int j=low;j<high;j++){
+        if(vec[j]<=pivot){
             i=i+1;
-        }
-        if(i<j){
             int temp=vec[i];
             vec[i]=vec[j];
             vec[j]=temp;
+            
         }
     }
-    int temp2=vec[low];
-    vec[low]=vec[j];
-    vec[j]=temp2;
-    return j;
+    
+    int temp2 = vec[i+1];
+    vec[i+1] = vec[high];
+    vec[high] = temp2;
+    return i+1;
 }
-void quickSort(std::vector<int> & vec,int low,int high){
-    if(low<high){
-        int pivot=partition(vec,low,high);
-      // std:: cout<<pivot;
-        quickSort(vec,low,pivot-1);
-        quickSort(vec,pivot+1,high);
-
+void quickSort(std::vector<int> & vec, int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(vec, low, high);
+        // std:: cout<<pivot;
+        quickSort(vec, low, pivot - 1);
+        quickSort(vec, pivot + 1, high);
     }
 }
