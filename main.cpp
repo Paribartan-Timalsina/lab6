c1898eb7796e8f6b5502d5b053aa5b42e3c00d50
Paribartan <timalsinapari015@gmail.com>
2022-11-27 21:14:18 +0545

    corrected the time

--- main.cpp
+++ main.cpp
@@ -3,35 +3,46 @@
 #include <time.h>
 #include <vector>
 #include <chrono>
+#include<fstream>
 using namespace std;
 using namespace std::chrono;
 #define MAX_SIZE 10000
 void quickSort(vector<int> &vec, int low, int high);
 int main()
 {
-  int j;
-  std::vector<int> vec;
-  srand(time(NULL));
-  for (j = MAX_SIZE; j <= 10 * MAX_SIZE; j = j + MAX_SIZE)
-  {
 
+  
+  
+  std::fstream file;
+  file.open("algorithmTime/csv",std::ios::out|std::ios::trunc);
+   file<<"Input size,QuickSort time\n";
+  
+  for ( int j = MAX_SIZE; j <= 10 * MAX_SIZE; j = j + MAX_SIZE)
+  {
+srand(time(0));
+vector<int> vec;
     for (int i = 0; i < j; i++)
     {
-      vec.push_back(rand() % 200);
+      int random=rand()%200;
+      vec.push_back(random);
     }
   //  cout<<"Unsorted data"<<endl;
   //   for(int i=0;i<j;i++){
   //   cout<<vec[i]<<endl;
   //   }
-    auto start = high_resolution_clock::now();
-   quickSort(vec, 0, j - 1);
-    auto stop = high_resolution_clock::now();
+      std::chrono ::steady_clock ::time_point start = std::chrono ::steady_clock ::now();
+   quickSort(vec, 0, vec.size()-1);
+  std::chrono ::steady_clock ::time_point stop = std::chrono ::steady_clock ::now();
 // cout<<"sorted data"<<endl;
 //     for(int i=0;i<j;i++){
 //     cout<<vec[i]<<endl;
 //     }
-    auto duration = duration_cast<microseconds>(stop - start);
+    auto duration = duration_cast<milliseconds>(stop - start);
+    int duration1=duration.count();
     cout << " (Quick Sort) Time taken by function: "
-         << duration.count()/1000<< " milliseconds" << " for the "<<j<<" values"<<endl;
+         << duration1<< " milliseconds" << " for the "<<j<<" values"<<endl;
+         file<<j << "," << duration1<<"\n";
+         file.flush();
   }
+
 }
