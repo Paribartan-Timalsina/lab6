ed0a5c9e961b9fca5bd39fb106ee7e2a39c9e30b
Paribartan <timalsinapari015@gmail.com>
2022-11-27 18:45:18 +0545

    corrected mistakes

--- sort.cpp
+++ sort.cpp
@@ -1,25 +1,73 @@
 #include <iostream>
 #include <vector>
-int partition(std::vector<int> & vec, int low, int high)
+int partition(std::vector<int> &vec, int low, int high)
 {
-    int pivot = vec[high];
-    int i = low-1;
-    for(int j=low;j<high;j++){
-        if(vec[j]<=pivot){
-            i=i+1;
-            int temp=vec[i];
-            vec[i]=vec[j];
-            vec[j]=temp;
-            
-        }
+    // int pivot = vec[low];
+    // int count = 0;
+    // int i = low, j = high;
+    // for (int loop = low + 1; loop <= high; loop++)
+    // {
+    //     if (vec[loop] <= pivot)
+    //     {
+    //         count++;
+    //     }
+    // }
+    // int pivotIndex = low + count;
+    // int temp = vec[pivotIndex];
+    // vec[pivotIndex] = vec[low];
+    // vec[low] = temp;
+   
+    // while (i < pivotIndex && j > pivotIndex)
+    // {
+
+    //     while (vec[i] <= pivot)
+    //     {
+    //         i++;
+    //     }
+
+    //     while (vec[j] > pivot)
+    //     {
+    //         j--;
+    //     }
+
+    //     if (i < pivotIndex && j > pivotIndex)
+    //     {
+           
+    //         int temp = vec[i++];
+    //         vec[i++] = vec[j--];
+    //         vec[j--] = temp;
+    //     }
+    // }
+    // return pivotIndex;
+    // select the rightmost element as pivot
+  int pivot = vec[low];
+  int i = low;
+  int j=high;
+  while(i<j) {
+    while(i<=high&&vec[i]<=pivot){
+        i++;
     }
-    
-    int temp2 = vec[i+1];
-    vec[i+1] = vec[high];
-    vec[high] = temp2;
-    return i+1;
+    while(j>=low&&vec[j]>=pivot){
+        if(j==low){
+            break;
+        }
+        j--;
+    }
+    if(i<j){
+        int temp=vec[i];
+        vec[i]=vec[j];
+        vec[j]=temp;
+    }
+  }
+    int temp2=vec[low];
+    vec[low]=vec[j];
+    vec[j]=temp2;
+    return j;
 }
-void quickSort(std::vector<int> & vec, int low, int high)
+
+
+
+void quickSort(std::vector<int> &vec, int low, int high)
 {
     if (low < high)
     {
